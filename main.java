4f39d36a84efc78e4d18a0c65034a085d88248e4
Shubham Kanje <shubham.kanje@gmail.com>
2022-03-12 12:18:21 +0530

    second commit

---
+++ main.java
@@ -0,0 +1,22 @@
+public class main {
+
+	public static void main(String[] args) 
+	{
+	       String name = "Hello world";
+	       System.out.println(name);
+	       
+	       int i=20;
+	       int j=20;
+	       
+	       int k=i+j;
+	       System.out.println("I="+i+"\nj="+ j +"\nk=" + k);
+	       
+	       for(int p=0; p<10; p++)
+	       {
+	    	       System.out.println("p="+ p);;
+	       }
+	       
+
+	}
+
+}
